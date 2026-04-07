bd82a229333f8550e5c8cdecfa6a106461847f56
Om Phule <omphule77@gmail.com>
2026-04-07 12:27:42 +0530

    Setup.py file is ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    try:
+        '''
+        this function will return the list of requirements
+        '''
+        requirements=[]
+        with open(file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
+
+            if '-e .' in requirements:
+                requirements.remove('-e .')
+    except FileNotFoundError:
+        print("File not found")
+    return requirements
+
+
+setup(
+    name='NetworkSecurity',
+    version='0.0.1',
+    author='Om Phulle',
+    author_email='omphule77@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
